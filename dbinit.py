111541804a16e2fd9f18646eb28cb1c0c0106894
Unknown <mehmet.caglayan8@gmail.com>
2018-12-15 11:36:14 +0300

    DELETE

--- dbinit.py
+++ dbinit.py
@@ -7,15 +7,10 @@ import psycopg2 as dbapi2
 INIT_STATEMENTS = [
     "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
     "INSERT INTO DUMMY VALUES (42)",
-	"ALTER TABLE problems DROP CONSTRAINT problems_user_id_fkey CASCADE",
-	"ALTER TABLE messages DROP CONSTRAINT  messages_clan_id_fkey CASCADE",
-	"ALTER TABLE messages DROP CONSTRAINT  messages_replyof_fkey CASCADE",
-	"ALTER TABLE messages DROP CONSTRAINT  messages_user_id_fkey CASCADE",
-	"ALTER TABLE clans DROP CONSTRAINT clans_headofclan_fkey CASCADE",
-	"DROP TABLE problems",
-	"DROP TABLE users",
-	"DROP TABLE messages",
-	"DROP clans"
+	"DROP TABLE problems CASCADE",
+	"DROP TABLE users CASCADE",
+	"DROP TABLE messages CASCADE",
+	"DROP clans CASCADE"
 ]
 
 
