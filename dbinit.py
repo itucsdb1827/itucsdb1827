0cf398dd9cae8b9ac2b4de9cb52fe571eda2592d
Unknown <mehmet.caglayan8@gmail.com>
2018-12-15 11:12:56 +0300

    delete tables

--- dbinit.py
+++ dbinit.py
@@ -7,12 +7,21 @@ import psycopg2 as dbapi2
 INIT_STATEMENTS = [
     "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
     "INSERT INTO DUMMY VALUES (42)",
-	"ALTER TABLE users DROP CONSTRAINT",
-	"ALTER TABLE problems DROP CONSTRAINT",
-	"ALTER TABLE messsages DROP CONSTRAINT",
+	"ALTER TABLE users DROP CONSTRAINT users_pkey",
+	"ALTER TABLE problems DROP CONSTRAINT problems_pkey",
+	"ALTER TABLE messsages DROP CONSTRAINT messages_pkey",
+	"ALTER TABLE users DROP CONSTRAINT users_pkey",
+	"ALTER TABLE users DROP CONSTRAINT users_clannumber_fkey",
+	"ALTER TABLE problems DROP CONSTRAINT messages_problem_id_fkey",
+	"ALTER TABLE problems DROP CONSTRAINT problems_user_id_fkey",
+	"ALTER TABLE messsages DROP CONSTRAINT  messages_clan_id_fkey",
+	"ALTER TABLE messsages DROP CONSTRAINT  messages_replyof_fkey",
+	"ALTER TABLE messsages DROP CONSTRAINT  messages_user_id_fkey",
+	"ALTER TABLE clans DROP CONSTRAINT clans_headofclan_fkey",
 	"DROP TABLE problems",
 	"DROP TABLE users",
 	"DROP TABLE messages",
+	"DROP clans"
 ]
 
 
